37b279a0c66af05be8a41f1ce8f1f5db5ac5b130
RacJes <rafael.candido88@gmail.com>
2020-12-30 20:26:23 -0300

    Criação do login + login para o sidebar

---
+++ admin/login.php
@@ -0,0 +1,51 @@
+<?php $nm_pagina = "BTeca - Login"; ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+        <title><?php echo $nm_pagina; ?></title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body style="background: linear-gradient(180deg, rgba(219,216,134,1) 13%, rgba(46,46,46,1) 100%);">
+<div class="container" >
+    
+        <div class="col-md-12 min-vh-100 d-flex flex-column justify-content-center">
+            <div class="col-lg-6 col-md-8 mx-auto">
+                <!-- form card login -->
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="text-center">
+                            Bibiloteca - Teca <br> Login
+                        </h3>
+                    </div>
+                    <div class="card-body">
+                        <form class="form" name="formLogin" action="#" method="POST">
+                            <div class="form-group">
+                                <label for="uname1">Username</label>
+                                <input type="text" class="form-control form-control-lg rounded-0" name="user" id="uname1" required="">
+                                <div class="invalid-feedback">por favor colocar l ogin</div>
+                            </div>
+                            <div class="form-group">
+                                <label>senha</label>
+                                <input type="password" class="form-control form-control-lg rounded-0" name="senha">
+                                <div class="invalid-feedback">
+                                Coloque sua senha.
+                                </div>
+                            </div>
+                            
+                            <button type="submit" class="btn btn-success btn-lg btn-block" id="btnLogin">Login</button>
+                        </form> 
+                    </div>
+                    <!--/card-block-->
+                </div>
+                <!-- /form card login -->
+            </div>
+        </div>
+        <!--/col-->
+</div>
+<!--/container--></html>
--- index.php
+++ index.php
@@ -53,7 +53,7 @@
                 <a href="#">Portfolio</a>
             </li>
             <li>
-                <a href="#">Contact</a>
+                <a href="admin/login.php">Login</a>
             </li>
         </ul>
     </nav>
@@ -69,28 +69,10 @@
                     <span></span>
                     <span></span>
                 </button>
-            
-                
-                <label class="sr-only" >Username</label>
                 <input type="text" class="form-control" placeholder="Pesquisa">
 
-                <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-align-justify"></i>
-                </button>
                 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    
-                <ul class="nav navbar-nav ml-auto">
-                        
-                        
-                        
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Login</a>
-                        </li>
-                        
-                    </ul>
-                </div>
-            </div>
+            
         </nav>
  <!-- Carrosel -->
  <center> <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
