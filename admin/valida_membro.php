d27bb64dc0a118e45e9859fbe6757f77f9d31b62
RacJes <rafael.candido88@gmail.com>
2021-01-24 01:40:44 -0300

    Valida Membro Update erro
    O insert e a validação do banco ta ok, mas ele ta com um erro no arquivo CRUD, não consegui identificar ainda, vou a mimir

--- admin/valida_membro.php
+++ admin/valida_membro.php
@@ -7,19 +7,32 @@
 <?php echo htmlspecialchars($_POST['files']); ?><br>
 <?php include("../conectar.php");
 
-
 $tabe="membro";
-$login="teste";
-$senha="senha1";
+$nome=$_POST['nome'];
+$cpf =$_POST['cpf'];
+$telefone =$_POST['telefone'];
+$endereco =$_POST['endereco'];
+$email=$_POST['email'];
+$arquivo =$_POST['files'];
+$nascimento =$_POST['nascimento'];
 
-$userCount	=	$db->numeroLinhas('login','idLogin');
+
+
+$userCount	=	$db->numeroLinhas($nome,$cpf,$telefone,$endereco,$email,$nascimento,$arquivo);
 $data	=	array(
-                'login'=>$login,
-                'senha'=>$senha,
+                'idmembro'=>"",
+                'nome'=>$nome,
+                'data_nacimento'=>$nascimemtno,
+                'cpf'=>$cpf,
+                'endereco'=>$endereco,
+                'telefone'=>$telefone,
+                'email'=>$email,
+                'imagem_idImagem'=>1,
+                'login_idLogin'=>1,
 );
 
 //"INSERT INTO $tableName (".implode(',', array_keys($data)).") VALUES (".implode(',', array_fill(0, count($data), '?')).")"
-$imprime1 =$db->InsertCrud($tabe,$data);
+$imprime1 =$db->InsertCrud($tabe,$data);    
 echo"<h1>$imprime1</h1>";
 $idlogin=$db->UtimoIDinserido();
 ?>
