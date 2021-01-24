2bba5cbad827f4283b0d6c1e0d19456abf26890f
RacJes <rafael.candido88@gmail.com>
2021-01-23 22:55:58 -0300

    tutorial

--- admin/valida_membro.php
+++ admin/valida_membro.php
@@ -7,5 +7,20 @@
 <?php echo htmlspecialchars($_POST['files']); ?><br>
 <?php include("../conectar.php");
 
+
+$tabe="membro";
+$login="teste";
+$senha="senha1";
+
+$userCount	=	$db->numeroLinhas('login','idLogin');
+$data	=	array(
+                'login'=>$login,
+                'senha'=>$senha,
+);
+
+//"INSERT INTO $tableName (".implode(',', array_keys($data)).") VALUES (".implode(',', array_fill(0, count($data), '?')).")"
+$imprime1 =$db->InsertCrud($tabe,$data);
+echo"<h1>$imprime1</h1>";
+$idlogin=$db->UtimoIDinserido();
 ?>
 
