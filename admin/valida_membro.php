2db988a598459c52fb5384d88890d84813b98e1b
RacJes <rafael.candido88@gmail.com>
2021-01-23 21:15:46 -0300

    Update valida_membro.php

--- admin/valida_membro.php
+++ admin/valida_membro.php
@@ -5,3 +5,14 @@
 <?php echo htmlspecialchars($_POST['email']); ?><br>
 <?php echo htmlspecialchars($_POST['nascimento']); ?><br>
 <?php echo htmlspecialchars($_POST['files']); ?><br>
+<?php include("../conectar.php");
+
+?>
+
+<?php 
+insert('Membro',$data){
+    $data= array(
+
+    );
+    
+}
